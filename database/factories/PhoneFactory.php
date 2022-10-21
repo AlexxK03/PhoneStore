b7550e71f7b141594be52aa273dc3a40fd2317e3
Alex Killian <N00212207@iadt.ie>
2022-10-21 12:02:08 +0100

    migration + model

--- database/factories/PhoneFactory.php
+++ database/factories/PhoneFactory.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\phone>
  */
-class phoneFactory extends Factory
+class PhoneFactory extends Factory
 {
     /**
      * Define the model's default state.
